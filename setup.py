e767b0d3ae283da2253781126b92cfa797a77802
ethan <ethan@tapwalk.com>
2013-02-04 16:40:20 -0500

    Setting up basic package structure, Kind of winging this based off a tutorial, may need to change. Gotta start somewhere

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='SPIMS',
+    version='0.1.0',
+    author='',
+    author_email='',
+    packages=['spims', 'spims.test'],
+    #url='http://pypi.python.org/pypi/TowelStuff/',
+    #license='LICENSE.txt',
+    description='Semester Project Image Match Search?',
+    long_description=open('README').read(),
+    #install_requires=[],
+)
