6d2626061d7e5082ebc9ab1d33394dff2ca4a362
ethan <ethan@tapwalk.com>
2013-02-06 17:38:21 -0500

    Add the test files from Assignment 4 directory. Kind of big but might be useful?

---
+++ spims/test/test_spims.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_loadImage(self):
+        pass
+
+    def test_spims(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
