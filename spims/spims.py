4dd5b1f7019fdc162132925335f8d13edfe020f9
ethan <ethan@tapwalk.com>
2013-02-06 19:30:27 -0500

    Get the option Parsing in, and some basic structure. You can run it and it will print filenames

---
+++ spims/spims.py
@@ -0,0 +1,24 @@
+import numpy, scipy
+from PIL import Image
+
+
+def match(pattern_file, source_file):
+    pattern = Image.open(pattern_file)
+    source = Image.open(source_file)
+    validate(pattern, source)
+    print "Pattern: " + pattern_file
+    print "Source: " + source_file
+
+
+def validate(pattern, source):
+    pattern.verify()
+    source.verify()
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-p", "--pattern", dest="pattern", help="Image to search for.", metavar="PATTERN_FILE")
+    parser.add_option("-s", "--source", dest="source", help="Image to search for", metavar="SOURCE_FILE")
+
+    (options, args) = parser.parse_args()
+    match(options.pattern, options.source)
