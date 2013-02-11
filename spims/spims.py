100392444b317fc9e920fb8b9d312765af070b74
Ethan A. Netland <enet@gauntlet.ccs.neu.edu>
2013-02-11 16:16:43 -0500

    outlined the matching process as I understand it so far, in comments. Gonna keep playing around with scipy

--- spims/spims.py
+++ spims/spims.py
@@ -10,14 +10,29 @@ import pdb
 
 
 def match(pattern_file, source_file):
-    pattern = Image.open(pattern_file).convert('L')
-    source = Image.open(source_file)
-    validate(pattern, source)
+    validate(pattern_file, source_file)
     print "Pattern: " + pattern_file
     print "Source: " + source_file
-
+    #First, Read in Both pattern and Source images
     pattern = sp.misc.imread(pattern_file, False)
+    source = sp.misc.imread(source_file, False)
+
+    #Take the fft of both Images, Padding out the smaller image with
+    # black to standardize the size
     pattern_fft = fftpack.fft2(pattern)
+
+    # Normalize the two arrays, should be like this:
+    # http://stackoverflow.com/questions/5639280/why-numpy-correlate-and-corrcoef-return-different-values-and-how-to-normalize
+    # a = (a - mean(a)) / (std(a) * len(a))
+    # v = (v - mean(v)) /  std(v) 
+    
+    
+    #Now we should be able to use signal.correlate2d to find the match location
+    # The output of this process is an array, I *think* the max value in this
+    # Array will be the upper left corner of the Match Location
+
+
+    #Some Testing Code
     pdb.set_trace()
     fft_power = np.log(np.abs(fftpack.fftshift(pattern_fft)) ** 2)
     Image.fromarray(fft_power.astype(np.uint8)).show()
@@ -28,9 +43,10 @@ def match(pattern_file, source_file):
     print source.shape
 
 
-def validate(pattern, source):
-    pattern.verify()
-    source.verify()
+def validate(pattern_file, source_file):
+    
+    Image.open(pattern_file).verify()
+    Image.open(source_file).verify()
 
 if __name__ == "__main__":
     from optparse import OptionParser
