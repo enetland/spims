9025d7a5bde771f91691a3fb7fc8a484bb7e6cc8
Chirag Khatri <ckhatri4@gmail.com>
2013-02-12 23:47:17 -0500

    Adds Img class so we don't have reopen image files multiple times, get dimensions, etc... Adds is_match method since algorithm returns match for any two matches given. This method is not full_proof and still needs work. Also did some overall refactor work

--- spims/spims.py
+++ spims/spims.py
@@ -8,63 +8,73 @@ import imghdr
 # This is just for debugging, should be removed later
 import pdb
 
+class Img:
+    def __init__(self, file_name):
+        self.full_name = file_name
+        self.name = os.path.basename(file_name)
+        self.data = sp.misc.imread(file_name, True)
+        self.height, self.width = self.data.shape
 
-def match(pattern_file, source_file):
-    
-    validate(pattern_file, source_file)
-    pattern = Image.open(pattern_file)
-    patternWidth, patternHeight = pattern.size
-    #First, Read in Both pattern and Source images
-    pattern = sp.misc.imread(pattern_file, True)
-    source = sp.misc.imread(source_file, True)
+    def is_valid_format(self):
+        return imghdr.what(self.full_name) == 'png' \
+                or imghdr.what(self.full_name) == 'gif' \
+                or imghdr.what(self.full_name) == 'jpeg'
 
+
+def match(pattern, source):
+
+    validate(pattern, source)
+
+    pattern_data = pattern.data
+    source_data = source.data
     # Normalize the two arrays, should be like this:
     # http://stackoverflow.com/questions/5639280/why-numpy-correlate-and-corrcoef-return-different-values-and-how-to-normalize
     # a = (a - mean(a)) / (std(a) * len(a))
     # v = (v - mean(v)) /  std(v)
-    normalized_pattern = (pattern - np.mean(pattern)) / (np.std(pattern) * pattern.size)
-    normalized_source = (source - np.mean(source)) / np.std(source)
+    normalized_pattern = (pattern_data - np.mean(pattern_data)) / (np.std(pattern_data) * pattern_data.size)
+    normalized_source = (source_data - np.mean(source_data)) / np.std(source_data)
 
-    #Take the fft of both Images, padding the pattern out with 0's to be the same shape as the source
-    pattern_fft = fftpack.fft2(normalized_pattern, source.shape)
+    #Take the fft of both Images, padding the pattern out with 0's
+    # to be the same shape as the source
+    pattern_fft = fftpack.fft2(normalized_pattern, source_data.shape)
     source_fft = fftpack.fft2(normalized_source)
 
-    # Perform the correlation in the frequency domain, which just the inverse FFT of the pattern matrix's conjugate *
-    # the source matrix
+    # Perform the correlation in the frequency domain, which just the
+    # inverse FFT of the pattern matrix's conjugate * the source matrix
     # http://en.wikipedia.org/wiki/Cross-correlation#Properties
     correlated = fftpack.ifft2(pattern_fft.conjugate() * source_fft) 
-    coords = np.unravel_index(correlated.argmax(), correlated.shape) 
-    # Find the Max of the correlated array, and print out the index in the source image
-    print os.path.basename(pattern_file) + " matches " + os.path.basename(source_file) + " at " + str(patternWidth) +\
-           "x" + str(patternHeight) + "+" + str(coords[1]) + "+" + str(coords[0])
-    # Can visualize the correlated matrix (For testing)
-    #Image.fromarray(correlated).show()
+    return np.unravel_index(correlated.argmax(), correlated.shape) 
 
+# If the coordinates returned by match are beyond the bounds of the
+# source image, a match was NOT found.
+def is_match(pattern, source, x, y):
+    return (x + pattern.width) <= source.width and \
+            (y + pattern.height) <= source.height
 
-def validate(pattern_file, source_file):
-    if not Image.open(pattern_file).verify() and Image.open(source_file).verify():
+def print_result(match_coords, pattern, source):
+    x = match_coords[1]
+    y = match_coords[0]
+    if is_match(pattern, source, x, y):
+        print "%s matches %s at %dx%d+%d+%d" % (pattern.name,
+                source.name, pattern.width, pattern.height, x, y)
+        # Can visualize the correlated matrix (For testing)
+        # Image.fromarray(correlated).show()
+
+def validate(pattern, source):
+    if not Image.open(pattern.full_name).verify() and Image.open(source.full_name).verify():
         raise Exception('Not a valid file!')
-    elif not check_format(pattern_file):
+    elif not pattern.is_valid_format():
         raise Exception('Pattern file is incorrect format')
-    elif not check_format(source_file):
+    elif not pattern.is_valid_format():
         raise Exception('Source file is incorrect format')
-    elif check_size(pattern_file, source_file):
+    elif check_size(pattern, source):
         raise Exception('Pattern file is larger than source file')
     else: 
         pass 
-    # Probably need a lot more verification here, ie. Pattern smaller than Source
-    # Not really sure how much the PIL verify function does either, the documentation
-    # sucks
 
-
-def check_format(img_file):
-    return imghdr.what(img_file) == 'png' or imghdr.what(img_file) == 'gif' or imghdr.what(img_file) == 'jpeg'
-
-def check_size(pattern_file, source_file):
-    pattern = Image.open(pattern_file)
-    source = Image.open(source_file)
-    return pattern.size > source.size
-
+def check_size(pattern, source):
+    return pattern.width > source.width or \
+            pattern.height > source.height
 
 if __name__ == "__main__":
     from optparse import OptionParser
@@ -78,4 +88,7 @@ if __name__ == "__main__":
     if options.source is None:
         parser.error('Source must be provided')
     else:
-        match(options.pattern, options.source)
+        pattern = Img(options.pattern)
+        source = Img(options.source)
+        match_coords = match(pattern, source)
+        print_result(match_coords, pattern, source)
